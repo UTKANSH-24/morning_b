cf4f3d483200c3bd6b799d8092a6ff450f70f200
Kunal Kumar <96365499+kunal23kmr@users.noreply.github.com>
2024-02-18 13:05:22 +0530

    Update user.controller.js

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -13,7 +13,7 @@ import Accommodation from '../models/accommodation.model.js';
 const cookieOptions = {
   secure: process.env.NODE_ENV === 'production' ? true : false,
   maxAge: 365 * 24 * 60 * 60 * 1000,
-  httpOnly: true,
+  // httpOnly: true,
 };
 
 
