0eadc31cb81476a2b5cbd6db8d51e7ec395af627
Kunal Kumar <kunal23kmr@gmail.com>
2024-02-19 23:58:54 +0530

    u r L

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.get('/ping', (_req, res) => {
 });
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin','http://localhost:3000');
+  res.header('Access-Control-Allow-Origin', process.env.FRONTEND_URL);
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.header('Access-Control-Allow-Credentials', 'true');
