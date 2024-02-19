928e0031dc37ccb78f7dc7b656f23db30be15af0
Kunal Kumar <kunal23kmr@gmail.com>
2024-02-19 21:53:35 +0530

    let's try to host.

--- app.js
+++ app.js
@@ -5,72 +5,56 @@ import morgan from 'morgan';
 import cookieParser from 'cookie-parser';
 import errorMiddleware from './middlewares/error.middleware.js';
 
-config(); // This should come after import { config } from 'dotenv';
+// Load environment variables from .env file
+config();
 
 const app = express();
 
+// Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// app.use(
-//   cors({
-//     origin: [process.env.FRONTEND_URL],
-//     credentials: true,
-//   })
-// );
 app.use(morgan('dev'));
 app.use(cookieParser());
-// Server Status Check Route
-app.get('/ping', (_req, res) => {
-  res.send('Pong');
-});
 
-
+// CORS Configuration
 const allowedOrigins = [
   "https://utkansh24.vercel.app",
   "http://localhost:3000",
 ];
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin',allowedOrigins);
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.header('Access-Control-Allow-Credentials', 'true');
+app.use(cors({
+  origin: allowedOrigins,
+  credentials: true,
+}));
 
-  // Handle preflight requests
-  if (req.method === 'OPTIONS') {
-    console.log(req.cookies);
-    res.sendStatus(200);
-  } else {
-    console.log('from app',JSON.stringify(req.cookies)+'\n');
+// Custom CORS handling for preflight requests
+app.options('*', cors());
 
-    next();
-  }
+// Server Status Check Route
+app.get('/ping', (_req, res) => {
+  res.send('Pong');
 });
 
-app.use(
-  cors({
-    origin: allowedOrigins,
-    credentials: true,
-  })
-);
-
-// Import all routes
+// Import routes
 import userRoutes from './routes/user.routes.js';
 import eventRoutes from './routes/event.routes.js';
 import miscRoutes from './routes/miscellaneous.routes.js';
 import merchandiseRoutes from './routes/merchandise.routes.js';
-import accommodation from './routes/accommodation.routes.js';
+import accommodationRoutes from './routes/accommodation.routes.js';
 
+// Route middleware
 app.use('/api/v1/user', userRoutes);
 app.use('/api/v1/event', eventRoutes);
 app.use('/api/v1', miscRoutes);
 app.use('/api/v1/merchandise', merchandiseRoutes);
-app.use('/api/v1/accommodation', accommodation);
+app.use('/api/v1/accommodation', accommodationRoutes);
 
 // Default catch all route - 404
 app.all('*', (_req, res) => {
   res.status(404).send('OOPS!!! 404 Page Not Found');
 });
+
 // Custom error handling middleware
 app.use(errorMiddleware);
+
 export default app;
