fbe4c2c30a656f1640daeca1f59d4dfc2cf8b06b
Kunal Kumar <kunal23kmr@gmail.com>
2024-02-20 18:51:16 +0530

    shree ganeshay namah

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ const allowedOrigins = [
   "http://localhost:3000",
 ];
 app.use(cors({
-  origin: "http://localhost:3000",
+  origin: "https://utkansh24.vercel.app",
   credentials: true,
 }));
 
