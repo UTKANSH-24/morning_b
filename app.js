82604bcfa29a1464d8f363eee38a7d7fd3a23d76
Kunal Kumar <kunal23kmr@gmail.com>
2024-02-20 22:23:49 +0530

    final

--- app.js
+++ app.js
@@ -1,35 +1,19 @@
+import cookieParser from 'cookie-parser';
+config();
 import express from 'express';
 import { config } from 'dotenv';
-import cors from 'cors';
 import morgan from 'morgan';
-import cookieParser from 'cookie-parser';
 import errorMiddleware from './middlewares/error.middleware.js';
-
-// Load environment variables from .env file
-config();
 
 const app = express();
 
-// Middleware
+// Middlewares
+// Built-In
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(morgan('dev'));
-app.use(cookieParser());
-
-// const allowedOrigins = [
-//   "https://utkansh24.vercel.app",
-//   "http://localhost:3000",
-// ];
-// app.use(cors({
-//   origin: 'https://utkansh24.vercel.app',
-//   credentials: true,
-// }));
-
-// // Custom CORS handling for preflight requests
-// app.options('*', cors());
-
+// Third-Party
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', "https://utkansh24.vercel.app" );
+  res.header('Access-Control-Allow-Origin', 'https://utkansh24.vercel.app');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.header('Access-Control-Allow-Credentials', 'true');
@@ -43,6 +27,8 @@ app.use((req, res, next) => {
 });
 
 
+app.use(morgan('dev'));
+app.use(cookieParser());
 
 // Server Status Check Route
 app.get('/ping', (_req, res) => {
@@ -56,7 +42,6 @@ import miscRoutes from './routes/miscellaneous.routes.js';
 import merchandiseRoutes from './routes/merchandise.routes.js';
 import accommodationRoutes from './routes/accommodation.routes.js';
 
-// Route middleware
 app.use('/api/v1/user', userRoutes);
 app.use('/api/v1/event', eventRoutes);
 app.use('/api/v1', miscRoutes);
@@ -65,7 +50,7 @@ app.use('/api/v1/accommodation', accommodationRoutes);
 
 // Default catch all route - 404
 app.all('*', (_req, res) => {
-  res.status(404).send('OOPS!!! 404 Page Not Found');
+  res.status(404).send('path does not exist:');
 });
 
 // Custom error handling middleware
