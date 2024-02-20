9f2de5617582e04013e69f7b48a0f29cbed201e9
Kunal Kumar <kunal23kmr@gmail.com>
2024-02-20 21:45:58 +0530

    shree ganeshay namah

--- app.js
+++ app.js
@@ -16,31 +16,31 @@ app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
 app.use(cookieParser());
 
-// const allowedOrigins = [
-//   "https://utkansh24.vercel.app",
-//   "http://localhost:3000",
-// ];
-// app.use(cors({
-//   origin: "https://utkansh24.vercel.app/",
-//   credentials: true,
-// }));
+const allowedOrigins = [
+  "https://utkansh24.vercel.app",
+  "http://localhost:3000",
+];
+app.use(cors({
+  origin: "https://utkansh24.vercel.app/",
+  credentials: true,
+}));
 
 // Custom CORS handling for preflight requests
 app.options('*', cors());
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', "https://utkansh24.vercel.app" );
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.header('Access-Control-Allow-Credentials', 'true');
+// app.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', "https://utkansh24.vercel.app" );
+//   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+//   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+//   res.header('Access-Control-Allow-Credentials', 'true');
 
-  // Handle preflight requests
-  if (req.method === 'OPTIONS') {
-    res.sendStatus(200);
-  } else {
-    next();
-  }
-});
+//   // Handle preflight requests
+//   if (req.method === 'OPTIONS') {
+//     res.sendStatus(200);
+//   } else {
+//     next();
+//   }
+// });
 
 
 
