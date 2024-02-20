a1b390424d2edad0cf678d46365327b71234a902
Kunal Kumar <kunal23kmr@gmail.com>
2024-02-20 21:42:12 +0530

    one more 3

--- app.js
+++ app.js
@@ -26,8 +26,8 @@ app.use(cors({
 }));
 
 app.use((req, res, next) => {
-  // res.header('Access-Control-Allow-Origin', "https://utkansh24.vercel.app" );
-  // res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Origin', "https://utkansh24.vercel.app" );
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.header('Access-Control-Allow-Credentials', 'true');
 
